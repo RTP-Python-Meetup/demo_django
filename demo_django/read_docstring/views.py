3a103feaba1d4a98c1b4291871e895f71beedc94
Malcolm VanOrder <mvanorder1390@gmail.com>
2020-07-29 00:07:10 -0400

    Add foo to context for template
    Signed-off-by: Malcolm VanOrder <mvanorder1390@gmail.com>

--- demo_django/read_docstring/views.py
+++ demo_django/read_docstring/views.py
@@ -5,3 +5,8 @@ class MainView(TemplateView):
 
     template_name = "read_docstring_main.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['foo'] = 'bar'
+
+        return context
